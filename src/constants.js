134239f0b520658ff51d132493e026f435618cf3
Martin Vaněk <nitram94@gmail.com>
2022-12-31 14:53:41 +0100

    breakpoints setup

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,9 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  phone: "max-width: 37.5rem",
+  tablet: "max-width: 59.375rem",
+  laptop: "max-width: 81.25rem"
+}
